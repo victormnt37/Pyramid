8b03d629b062641b2e21ccb45cee987fe50772ee
victormnt37 <victormorantf@gmail.com>
2022-04-26 17:21:23 +0200

    botones funcionales

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const input3 = document.querySelector('div#three input');
+
+/************ ADJUSTMENTS AND INFO ************/
+
 const info = document.querySelector('div#info');
 const infoButton = document.querySelector('i.fa-solid.fa-circle-question');
 const infoEsc = document.querySelector('div#info button');
@@ -25,4 +29,23 @@ adjustButton.addEventListener('click', () => {
 
 adjustEsc.addEventListener('click', () => {
   adjust.hidden = true;
+});
+
+/************ LETTER'S BUTTONS ************/
+
+const letter1 = document.querySelector('button#letter1');
+const letter2 = document.querySelector('button#letter2');
+const letter3 = document.querySelector('button#letter3');
+const letter4 = document.querySelector('button#letter4');
+const letter5 = document.querySelector('button#letter5');
+const letter6 = document.querySelector('button#letter6');
+const letter7 = document.querySelector('button#letter7');
+
+letter1.addEventListener('click', () => {
+  //El input3 se deberá cambiar por un "document.querySelector('input.selected')".
+  //esto permite que se identifique automáticamente cual es el input en el que se debe escribir en esa ocasión
+  if (document.querySelector('div.selected input').value.length < 3) {
+    document.querySelector('div.selected input').value =
+      document.querySelector('div.selected input').value + letter1.innerHTML;
+  }
 });
